653702504141eb42350fb77268ea18a024595497
Devang Gaur <devang.only@gmail.com>
2016-03-28 23:29:32 +0530

    User needs their own api key now

--- thesaurus.js
+++ thesaurus.js
@@ -5,30 +5,32 @@ var obj={
             type : []
         };
 
-module.exports=function(word,cb){
-    
-    unirest.get("http://words.bighugelabs.com/api/2/179b92829817af021068cae049a071ea/"+word.trim()+"/json")
+module.exports = function(word, cb, api_key) {
+
+    if( !api_key ) {
+        throw new Error('Inappropriate or no API key found');
+        cb(obj);
+    }
+
+    unirest.get("http://words.bighugelabs.com/api/2/"+api_key+"/"+word.trim()+"/json")
     .header("Accept", "application/json")
     .end(function (res) {
-
-
-          if(res.status==200)
-          {
+        if(res.status==200)
+        {
             obj.found=true;
-            
             var result=JSON.parse(res.body);
-            
+
             for(var i in result)
             {
                 obj.type.push(i);
                 obj.type[i]={synonyms:[],antonyms:[]};
-                
+
                 for(var j in result[i])
                 {
                     if(j=="syn")
                     {
                         for(var k=0 in result[i][j])
-                        {   
+                        {
                             obj.type[i].synonyms.push(result[i][j][k]);
                         }
                     }
@@ -41,26 +43,22 @@ module.exports=function(word,cb){
                     }
                 }
             }
-            
-          }
-          else
-          {
-           obj.found=false;
-          }
-
+        }
+        else
+        {
+            obj.found=false;
+        }
 
         var temp=[];
+
         for(var i in obj.type)
         {
             if(obj.type[i] instanceof Object)
             {
                 temp[i]=obj.type[i];
             }
-
         }
         obj.type=temp;
-
         cb(obj);
-
     });
 };
